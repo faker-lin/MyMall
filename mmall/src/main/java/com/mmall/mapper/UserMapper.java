fe758b4e7768ceab2dcea07e6bb4a92c2c93596a
facker-lin <353439198@qq.com>
2018-07-18 15:23:34 +0800

    second

---
+++ mmall/src/main/java/com/mmall/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.mmall.mapper;
+
+import com.mmall.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
---
+++ mmall/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+我这次是在idea上改的
+</body>
+</html>
